f5c17f82b209af0d2a1e9385e09fd6a7eed5cc1e
Giuseppe D'Agostino <g.a.dagostino@gmail.com>
2022-06-09 10:24:28 +0800

    several changes
    - added custom error/warning messages: now the message changes based on whether the function throws an error or a message
    - new sysdata.rda
    - updated documentation
    - updated DESCRIPTION

--- R/pixpals.R
+++ R/pixpals.R
@@ -1,8 +1,10 @@
 pixpals = list()
 
 pixpals$Buster = list("crayon" = makePixPal("inst/extdata/buster.png"),
-                         "messages" = c("Hold the bus!",
-                                        "Busted!",
+
+                         "messages" = list(
+
+                            "Error" = c("Busted!",
                                         "When life gives you lemons, I give you",
                                         "Whoa there cowboy!",
                                         "How 'bout dem lemons?",
@@ -10,11 +12,22 @@ pixpals$Buster = list("crayon" = makePixPal("inst/extdata/buster.png"),
                                         "Someone is having a case of the Mondays!",
                                         "Jet fuel doesn't melt steel bea- I mean holy guacamole!",
                                         "Slow there kiddo!",
-                                        "\"You miss 100% of the shots you don't take\"- Wayne Gretzky -- Michael Scott\""))
+                                        "\"You miss 100% of the shots you don't take\"- Wayne Gretzky -- Michael Scott\""),
+
+                            "Warning" = c("I'm getting some vibes here!",
+                                         "The more you know, amirite?",
+                                         "I'll take \"Potential causes of concern\" for $500, Alex.",
+                                         "Something smells fishy around here...",
+                                         "Hold the bus!",
+                                         "I'll let you deal with this, pal!")
+                            )
+                      )
 
 pixpals$Jerry = list("crayon" = makePixPal("inst/extdata/jerry.png"),
-                     "messages" = c("Squawk!",
-                                    "Sounds like you should learn to code.",
+
+                     "messages" = list(
+
+                        "Error" = c("Sounds like you should learn to code.",
                                     "Pathetic.",
                                     "How many times do we need to go through this?",
                                     "You know you will just look this up on StackOverflow, so why bother?",
@@ -25,21 +38,45 @@ pixpals$Jerry = list("crayon" = makePixPal("inst/extdata/jerry.png"),
                                     "Learning from your mistakes implies learning at some point.",
                                     "I'd say I'm disappointed but I'll leave that to the school system.",
                                     "Fail upwards, my friend!",
-                                    "Did I ever tell you I'm proud of you? No? Yeah, this is why."))
+                                    "Did I ever tell you I'm proud of you? No? Yeah, this is why.",
+                                    "Admit it: you copy-pasted this.",
+                                    "This code is so bad you should mint it as an NFT."),
+
+                      "Warning" = c("You're on thin ice and the cracks are getting louder.",
+                                    "I'm sure this would have been an error if someone didn't whine too much.",
+                                    "Of course you are going to ignore this.",
+                                    "Warnings are just errors that have pity on you.",
+                                    "Well that's surprising. And by that I mean the fact that it didn't throw an error.",
+                                    "Yeah, keep pushing, I'm sure it won't break and you won't be in trouble.",
+                                    "Newsflash: you almost screwed up. ")
+                      )
+                     )
 
 pixpals$Oniji = list("crayon" = makePixPal("inst/extdata/oniji.png"),
-                     "messages" = c("A single function / Mysterious operations / It has failed again.",
-                                    "That time in Kyoto / Coding, as cherry blossom / Shelters many bugs.",
-                                    "The first reviewer / May never catch this issue / But surely I did.",
-                                    "Tracing back the source /  May reveal all the answers / Though sometimes it won't.",
-                                    "Showing just one side /  The moon is ever careful / Unlike who wrote this.",
-                                    "Solitary bee /  A quiet buzz through my orchids / Another bug shows.",
-                                    "Wandering Basho /  Observing all conventions / Would never do this.",
-                                    "Another error /  We proceed stil undeterred / Ready for failure."))
+
+                     "messages" = list(
+                       "Error" = c("A single function / Mysterious operations / It has failed again.",
+                                   "That time in Kyoto / Coding, as cherry blossom / Shelters many bugs.",
+                                   "The first reviewer / May never catch this issue / But surely I did.",
+                                   "Tracing back the source /  May reveal all the answers / Though sometimes it won't.",
+                                   "Showing just one side /  The moon is ever careful / Unlike who wrote this.",
+                                   "Solitary bee /  A quiet buzz through my orchids / Another bug shows.",
+                                   "Wandering Basho /  Observing all conventions / Would never do this.",
+                                   "Another error /  We proceed stil undeterred / Ready for failure."),
+                       "Warning" = c("Cold Hokkaido wind / a sparrow shakes the snow off / now you have been warned.",
+                                     "Fires in the night / somewhere, something has happened / do not ignore it.",
+                                     "The looming feeling / Of a potential failure / For now you are safe.",
+                                     "Sleeping Fujisan / One day it will awaken / Better be careful.",
+                                     "Salted plum rice balls / sweet at first, then surprising / as this warning is.",
+                                     "Fall in Hakone / leaves shaking red and yellow / Frail just like this code.")
+                       )
+                     )
 
 
 pixpals$Lenny = list("crayon" = makePixPal("inst/extdata/lenny.png"),
-                     "messages" = c("You got this!",
+
+                     "messages" = list(
+                       "Error" = c("You got this!",
                                     "Please don't mind this pile of bones while I say GOOD ATTEMPT!",
                                     "Don't let small mistakes distract you from your incredibly important mission!",
                                     "I'm sure it almost never happens!",
@@ -52,4 +89,13 @@ pixpals$Lenny = list("crayon" = makePixPal("inst/extdata/lenny.png"),
                                     "You're my best friend even if your code isn't!",
                                     "Even the best coders in the world make mistakes, and who said you're not one of them?",
                                     "Never give up! It makes the chase more excit- I mean, you deserve to win!",
-                                    "I'll tell you what I told the parents of those kids when the cage was left open: sometimes mistakes happen."))
+                                    "I'll tell you what I told the parents of those kids when the cage was left open: sometimes mistakes happen."),
+
+                       "Warning" = c("Be careful my friend! The only warning signs you should ignore are the ones around my cage.",
+                                   "Nothing to worry about! Keep going strong!",
+                                   "I'm sure it's all gona be okay!",
+                                   "Warnings are nature's ways of telling us we have another chance!",
+                                   "Not so bad! Could have been worse!",
+                                   "As long as you don't ignore this warning you will be A-OK, amigo!")
+                       )
+                  )
